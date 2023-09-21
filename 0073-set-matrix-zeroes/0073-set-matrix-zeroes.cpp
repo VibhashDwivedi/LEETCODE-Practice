ab8f41f1bec9244ba99bf51e83bbff243f3f3f27
Vibhash Dwivedi <94799645+VibhashDwivedi@users.noreply.github.com>
2023-09-21 18:27:38 +0530

    Time: 12 ms (58.44%), Space: 13.9 MB (6.64%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,45 @@
+
+        class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        set<int>row;
+        set<int>col;
+        int n= matrix.size();
+        int m= matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row.insert(i);
+                    col.insert(j);
+                    
+                }
+            }
+        }
+        
+        for( auto it=row.begin();it!=row.end();it++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                matrix[*it][j]=0;
+                    
+            }
+        }
+        for( auto it=col.begin();it!=col.end();it++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[j][*it]=0;
+                    
+            }
+        }
+        
+        
+        
+        
+    }
+};
+  
