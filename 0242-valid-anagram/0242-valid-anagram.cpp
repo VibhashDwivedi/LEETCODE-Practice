b3d3a0f60ccfd26f04ec0978c42e7cc9ed5db102
Vibhash Dwivedi <94799645+VibhashDwivedi@users.noreply.github.com>
2023-09-23 21:25:16 +0530

    Time: 8 ms (63.92%), Space: 7.9 MB (5.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,18 +2,46 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        vector<int> check(26,0);
+//         vector<int> check(26,0);
+//         for(int i=0;i<s.length();i++)
+//             check[s[i]-'a']++;
+        
+//         for(int i=0;i<t.length();i++)
+//             check[t[i]-'a']--;
+//         for(int i=0;i<26;i++)
+//         {
+//             if(check[i]!=0)
+//                 return false;
+//         }
+        
+//         return true;
+        
+        
+        
+        unordered_map<char, int>mp;
+        
         for(int i=0;i<s.length();i++)
-            check[s[i]-'a']++;
+        {
+            mp[s[i]]++;
+        }
+        
         
         for(int i=0;i<t.length();i++)
-            check[t[i]-'a']--;
-        for(int i=0;i<26;i++)
         {
-            if(check[i]!=0)
+            if(mp.find(t[i]) != mp.end())
+            {
+                if(mp[t[i]]>1)
+                    mp[t[i]]--;
+                else
+                    mp.erase(t[i]);
+            }
+            else
                 return false;
         }
         
-        return true;
+        if(mp.empty())
+            return true;
+        else
+            return false;
     }
 };
